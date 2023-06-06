08b69304e4cd3f3a23f36938412da1ac1b98815f
marko <cenga.marko@gmail.com>
2023-06-06 16:16:45 +0200

    test

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -93,7 +93,7 @@ def evaluate_semseg(model, data_loader, class_info, observers=(), anomaly_loader
         for ctx_mgr in managers:
             stack.enter_context(ctx_mgr)
         conf_mat = np.zeros((model.num_classes, model.num_classes), dtype=np.uint64)
-        print("AAAAAAAAAAAAAAAA")
+        print("AAAAAAAAAAAAAAAA", len(data_loader))
         for step, batch in tqdm(enumerate(data_loader), total=len(data_loader)):
             batch['original_labels'] = batch['original_labels'].numpy().astype(np.uint32)
             logits, additional = model.do_forward(batch, batch['original_labels'].shape[1:3])
