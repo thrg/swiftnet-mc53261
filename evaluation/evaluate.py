f197fe508a8d7b675cb860c0fc907be29510da27
marko <cenga.marko@gmail.com>
2023-06-07 16:20:37 +0200

    ignore class not ignored

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -83,8 +83,8 @@ def evaluate_anomaly(model, data_loader, anomaly_function):
     score = np.array(score)
     print(gt[0])
     print(score[0])
-    ap = average_precision_score(gt[gt != 255], score[gt != 255])
-    auroc = roc_auc_score(gt[gt != 255], score[gt != 255])
+    ap = average_precision_score(gt, score)
+    auroc = roc_auc_score(gt, score)
     return ap, auroc
 
 
