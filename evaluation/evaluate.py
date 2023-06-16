af842b175b7a39b920d9d3f18867e66784c83296
marko <cenga.marko@gmail.com>
2023-06-16 14:07:53 +0200

    just one hist softmax not anomaly

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -94,8 +94,8 @@ def evaluate_anomaly(model, data_loader):
     entropy_ap = []
     entropy_auroc = []
 
-    # softmax_scores = []
-    # logit_scores = []
+    softmax_scores = []
+    logit_scores = []
     entropy_scores = []
 
     with contextlib.ExitStack() as stack:
@@ -106,69 +106,69 @@ def evaluate_anomaly(model, data_loader):
             new_gt = gt[gt != 2]
             logits, additional = model.do_forward(batch, batch['original_labels'].shape[1:3])
 
-            # score = max_softmax(logits.data).cpu().numpy()
-            # score = score[gt != 2]
-            # softmax_scores.extend(score.tolist())
-            # if 0 in new_gt and 1 in new_gt:
-                # softmax_ap.append(average_precision_score(new_gt, score))
-            #     roc_display = RocCurveDisplay.from_predictions(new_gt, score)
-            #     roc_display.plot()
-            #     plt.savefig(f"images/auroc_softmax_{step}")
-            #     plt.close()
-            #     ap_display = PrecisionRecallDisplay.from_predictions(new_gt, score)
-            #     ap_display.plot()
-            #     plt.savefig(f"images/ap_softmax_{step}")
-            #     plt.close()
-            #     softmax_auroc.append(roc_auc_score(new_gt, score))
+            score = max_softmax(logits.data).cpu().numpy()
+            score = score[gt != 2]
+            softmax_scores.extend(score.tolist())
+            if 0 in new_gt and 1 in new_gt:
+                softmax_ap.append(average_precision_score(new_gt, score))
+                roc_display = RocCurveDisplay.from_predictions(new_gt, score)
+                roc_display.plot()
+                plt.savefig(f"images/auroc_softmax_{step}")
+                plt.close()
+                ap_display = PrecisionRecallDisplay.from_predictions(new_gt, score)
+                ap_display.plot()
+                plt.savefig(f"images/ap_softmax_{step}")
+                plt.close()
+                softmax_auroc.append(roc_auc_score(new_gt, score))
 
-            # score = max_logit(logits.data).cpu().numpy()
-            # score = score[gt != 2]
-            # logit_scores.extend(score.tolist())
-            # if 0 in new_gt and 1 in new_gt:
-                # logit_ap.append(average_precision_score(new_gt, score))
-            #     roc_display = RocCurveDisplay.from_predictions(new_gt, score)
-            #     roc_display.plot()
-            #     plt.savefig(f"images/auroc_logit_{step}")
-            #     plt.close()
-            #     ap_display = PrecisionRecallDisplay.from_predictions(new_gt, score)
-            #     ap_display.plot()
-            #     plt.savefig(f"images/ap_logit_{step}")
-            #     plt.close()
-            #     logit_auroc.append(roc_auc_score(new_gt, score))
+            score = max_logit(logits.data).cpu().numpy()
+            score = score[gt != 2]
+            logit_scores.extend(score.tolist())
+            if 0 in new_gt and 1 in new_gt:
+                logit_ap.append(average_precision_score(new_gt, score))
+                roc_display = RocCurveDisplay.from_predictions(new_gt, score)
+                roc_display.plot()
+                plt.savefig(f"images/auroc_logit_{step}")
+                plt.close()
+                ap_display = PrecisionRecallDisplay.from_predictions(new_gt, score)
+                ap_display.plot()
+                plt.savefig(f"images/ap_logit_{step}")
+                plt.close()
+                logit_auroc.append(roc_auc_score(new_gt, score))
 
             score = entropy(logits.data).cpu().numpy()
             score = score[gt != 2]
             entropy_scores.extend(score.tolist())
-            # if 0 in new_gt and 1 in new_gt:
-                # entropy_ap.append(average_precision_score(new_gt, score))
-            #     roc_display = RocCurveDisplay.from_predictions(new_gt, score)
-            #     roc_display.plot()
-            #     plt.savefig(f"images/auroc_entropy_{step}")
-            #     plt.close()
-            #     ap_display = PrecisionRecallDisplay.from_predictions(new_gt, score)
-            #     ap_display.plot()
-            #     plt.savefig(f"images/ap_entropy_{step}")
-            #     plt.close()
-            #     entropy_auroc.append(roc_auc_score(new_gt, score))
+            if 0 in new_gt and 1 in new_gt:
+                entropy_ap.append(average_precision_score(new_gt, score))
+                roc_display = RocCurveDisplay.from_predictions(new_gt, score)
+                roc_display.plot()
+                plt.savefig(f"images/auroc_entropy_{step}")
+                plt.close()
+                ap_display = PrecisionRecallDisplay.from_predictions(new_gt, score)
+                ap_display.plot()
+                plt.savefig(f"images/ap_entropy_{step}")
+                plt.close()
+                entropy_auroc.append(roc_auc_score(new_gt, score))
 
         print('')
     model.train()
 
-    # softmax_scores = np.array(softmax_scores)
-    # logit_scores = np.array(logit_scores)
+    softmax_scores = np.array(softmax_scores)
+    logit_scores = np.array(logit_scores)
     entropy_scores = np.array(entropy_scores)
 
-    # plt.hist(softmax_scores)
-    # plt.xlabel('Vrijednost anomalije piksela')
-    # plt.ylabel('Broj piksela')
-    # plt.savefig(f"images/hist_softmax_anomaly")
-    # plt.close()
+    plt.hist(softmax_scores)
+    plt.xlabel('Vrijednost anomalije piksela')
+    plt.ylabel('Broj piksela')
+    plt.savefig(f"images/hist_softmax_anomaly")
+    plt.close()
 
-    # plt.hist(logit_scores)
-    # plt.xlabel('Vrijednost anomalije piksela')
-    # plt.ylabel('Broj piksela')
-    # plt.savefig(f"images/hist_logit_anomaly")
-    # plt.close()
+    plt.hist(logit_scores)
+    plt.xlabel('Vrijednost anomalije piksela')
+    plt.ylabel('Broj piksela')
+    plt.savefig(f"images/hist_logit_anomaly")
+    plt.close()
 
     plt.hist(entropy_scores)
     plt.xlabel('Vrijednost anomalije piksela')
@@ -204,11 +204,11 @@ def evaluate_semseg(model, data_loader, class_info, observers=()):
     with contextlib.ExitStack() as stack:
         for ctx_mgr in managers:
             stack.enter_context(ctx_mgr)
-        conf_mat = np.zeros((model.num_classes, model.num_classes), dtype=np.uint64)
+        # conf_mat = np.zeros((model.num_classes, model.num_classes), dtype=np.uint64)
         for step, batch in tqdm(enumerate(data_loader), total=len(data_loader)):
             batch['original_labels'] = batch['original_labels'].numpy().astype(np.uint32)
             logits, additional = model.do_forward(batch, batch['original_labels'].shape[1:3])
-            pred = torch.argmax(logits.data, dim=1).byte().cpu().numpy().astype(np.uint32)
+            # pred = torch.argmax(logits.data, dim=1).byte().cpu().numpy().astype(np.uint32)
 
             score = max_softmax(logits.data).cpu().numpy()
             score = score[batch['original_labels'] != 2]
@@ -222,11 +222,11 @@ def evaluate_semseg(model, data_loader, class_info, observers=()):
             # score = score[batch['original_labels'] != 2]
             # entropy_scores.extend(score.tolist())
 
-            for o in observers:
-                o(pred, batch, additional)
-            calculate_conf_matrix(pred.flatten(), batch["original_labels"].flatten(), conf_mat, model.num_classes)
+            # for o in observers:
+            #     o(pred, batch, additional)
+            # calculate_conf_matrix(pred.flatten(), batch["original_labels"].flatten(), conf_mat, model.num_classes)
         print('')
-        pixel_acc, iou_acc, recall, precision, _, per_class_iou = compute_errors(conf_mat, class_info, verbose=True)
+        # pixel_acc, iou_acc, recall, precision, _, per_class_iou = compute_errors(conf_mat, class_info, verbose=True)
     model.train()
 
     softmax_scores = np.array(softmax_scores)
@@ -251,7 +251,8 @@ def evaluate_semseg(model, data_loader, class_info, observers=()):
     # plt.savefig(f"images/hist_entropy_normal")
     # plt.close()
 
-    return iou_acc, per_class_iou
+    # return iou_acc, per_class_iou
+    return 0, 0
 
 
 def calculate_conf_matrix(predicted, target, conf_mat, num_classes=12):
