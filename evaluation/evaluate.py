9d440de80313c45d302f0e52119a64e48e15a297
marko <cenga.marko@gmail.com>
2023-06-16 13:36:36 +0200

    just one hist

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -95,8 +95,8 @@ def evaluate_anomaly(model, data_loader):
     entropy_auroc = []
 
     softmax_scores = []
-    logit_scores = []
-    entropy_scores = []
+    # logit_scores = []
+    # entropy_scores = []
 
     with contextlib.ExitStack() as stack:
         for ctx_mgr in managers:
@@ -121,9 +121,9 @@ def evaluate_anomaly(model, data_loader):
             #     plt.close()
             #     softmax_auroc.append(roc_auc_score(new_gt, score))
 
-            score = max_logit(logits.data).cpu().numpy()
-            score = score[gt != 2]
-            logit_scores.extend(score.tolist())
+            # score = max_logit(logits.data).cpu().numpy()
+            # score = score[gt != 2]
+            # logit_scores.extend(score.tolist())
             # if 0 in new_gt and 1 in new_gt:
                 # logit_ap.append(average_precision_score(new_gt, score))
             #     roc_display = RocCurveDisplay.from_predictions(new_gt, score)
@@ -136,9 +136,9 @@ def evaluate_anomaly(model, data_loader):
             #     plt.close()
             #     logit_auroc.append(roc_auc_score(new_gt, score))
 
-            score = entropy(logits.data).cpu().numpy()
-            score = score[gt != 2]
-            entropy_scores.extend(score.tolist())
+            # score = entropy(logits.data).cpu().numpy()
+            # score = score[gt != 2]
+            # entropy_scores.extend(score.tolist())
             # if 0 in new_gt and 1 in new_gt:
                 # entropy_ap.append(average_precision_score(new_gt, score))
             #     roc_display = RocCurveDisplay.from_predictions(new_gt, score)
@@ -155,8 +155,8 @@ def evaluate_anomaly(model, data_loader):
     model.train()
 
     softmax_scores = np.array(softmax_scores)
-    logit_scores = np.array(logit_scores)
-    entropy_scores = np.array(entropy_scores)
+    # logit_scores = np.array(logit_scores)
+    # entropy_scores = np.array(entropy_scores)
 
     plt.hist(softmax_scores)
     plt.xlabel('Vrijednost anomalije piksela')
@@ -164,17 +164,17 @@ def evaluate_anomaly(model, data_loader):
     plt.savefig(f"images/hist_softmax_anomaly")
     plt.close()
 
-    plt.hist(logit_scores)
-    plt.xlabel('Vrijednost anomalije piksela')
-    plt.ylabel('Broj piksela')
-    plt.savefig(f"images/hist_logit_anomaly")
-    plt.close()
-
-    plt.hist(entropy_scores)
-    plt.xlabel('Vrijednost anomalije piksela')
-    plt.ylabel('Broj piksela')
-    plt.savefig(f"images/hist_entropy_anomaly")
-    plt.close()
+    # plt.hist(logit_scores)
+    # plt.xlabel('Vrijednost anomalije piksela')
+    # plt.ylabel('Broj piksela')
+    # plt.savefig(f"images/hist_logit_anomaly")
+    # plt.close()
+    #
+    # plt.hist(entropy_scores)
+    # plt.xlabel('Vrijednost anomalije piksela')
+    # plt.ylabel('Broj piksela')
+    # plt.savefig(f"images/hist_entropy_anomaly")
+    # plt.close()
 
     softmax_ap = np.array(softmax_ap)
     softmax_auroc = np.array(softmax_auroc)
@@ -198,8 +198,8 @@ def evaluate_semseg(model, data_loader, class_info, observers=()):
     managers = [torch.no_grad()] + list(observers)
 
     softmax_scores = []
-    logit_scores = []
-    entropy_scores = []
+    # logit_scores = []
+    # entropy_scores = []
 
     with contextlib.ExitStack() as stack:
         for ctx_mgr in managers:
@@ -214,13 +214,13 @@ def evaluate_semseg(model, data_loader, class_info, observers=()):
             score = score[batch['original_labels'] != 2]
             softmax_scores.extend(score.tolist())
 
-            score = max_logit(logits.data).cpu().numpy()
-            score = score[batch['original_labels'] != 2]
-            logit_scores.extend(score.tolist())
-
-            score = entropy(logits.data).cpu().numpy()
-            score = score[batch['original_labels'] != 2]
-            entropy_scores.extend(score.tolist())
+            # score = max_logit(logits.data).cpu().numpy()
+            # score = score[batch['original_labels'] != 2]
+            # logit_scores.extend(score.tolist())
+            #
+            # score = entropy(logits.data).cpu().numpy()
+            # score = score[batch['original_labels'] != 2]
+            # entropy_scores.extend(score.tolist())
 
             for o in observers:
                 o(pred, batch, additional)
@@ -230,8 +230,8 @@ def evaluate_semseg(model, data_loader, class_info, observers=()):
     model.train()
 
     softmax_scores = np.array(softmax_scores)
-    logit_scores = np.array(logit_scores)
-    entropy_scores = np.array(entropy_scores)
+    # logit_scores = np.array(logit_scores)
+    # entropy_scores = np.array(entropy_scores)
 
     plt.hist(softmax_scores)
     plt.xlabel('Vrijednost anomalije piksela')
@@ -239,17 +239,17 @@ def evaluate_semseg(model, data_loader, class_info, observers=()):
     plt.savefig(f"images/hist_softmax_normal")
     plt.close()
 
-    plt.hist(logit_scores)
-    plt.xlabel('Vrijednost anomalije piksela')
-    plt.ylabel('Broj piksela')
-    plt.savefig(f"images/hist_logit_normal")
-    plt.close()
-
-    plt.hist(entropy_scores)
-    plt.xlabel('Vrijednost anomalije piksela')
-    plt.ylabel('Broj piksela')
-    plt.savefig(f"images/hist_entropy_normal")
-    plt.close()
+    # plt.hist(logit_scores)
+    # plt.xlabel('Vrijednost anomalije piksela')
+    # plt.ylabel('Broj piksela')
+    # plt.savefig(f"images/hist_logit_normal")
+    # plt.close()
+    #
+    # plt.hist(entropy_scores)
+    # plt.xlabel('Vrijednost anomalije piksela')
+    # plt.ylabel('Broj piksela')
+    # plt.savefig(f"images/hist_entropy_normal")
+    # plt.close()
 
     return iou_acc, per_class_iou
 
